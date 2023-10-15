eeb6960b32c6dd2866bad8eb9a932017726489bb
idooru <shere1765@gmail.com>
2023-10-15 13:00:49 +0900

    json 응답을 가시화

--- src/main/java/com/example/kakao_api_tester/data/type/SearchResponseJSON.java
+++ src/main/java/com/example/kakao_api_tester/data/type/SearchResponseJSON.java
@@ -1,20 +1,41 @@
 package com.example.kakao_api_tester.data.type;
 
-class SearchResponseKey {
-    public String address_name;
-    public String category_group_code;
-    public String category_group_name;
-    public String category_name;
-    public String distance;
-    public String id;
-    public String phone;
-    public String place_name;
-    public String place_url;
-    public String road_address_name;
-    public String x;
-    public String y;
-}
+import java.util.List;
 
 public class SearchResponseJSON {
-    public SearchResponseKey[] documents;
+
+    public static class SearchResponseKey {
+        public String address_name;
+        public String category_group_code;
+        public String category_group_name;
+        public String category_name;
+        public String distance;
+        public String id;
+        public String phone;
+        public String place_name;
+        public String place_url;
+        public String road_address_name;
+        public String x;
+        public String y;
+
+        @Override
+        public String toString() {
+            return "SearchResponseKey{" +
+                    "address_name='" + address_name + '\'' +
+                    ", category_group_code='" + category_group_code + '\'' +
+                    ", category_group_name='" + category_group_name + '\'' +
+                    ", category_name='" + category_name + '\'' +
+                    ", distance='" + distance + '\'' +
+                    ", id='" + id + '\'' +
+                    ", phone='" + phone + '\'' +
+                    ", place_name='" + place_name + '\'' +
+                    ", place_url='" + place_url + '\'' +
+                    ", road_address_name='" + road_address_name + '\'' +
+                    ", x='" + x + '\'' +
+                    ", y='" + y + '\'' +
+                    '}';
+        }
+    }
+
+    public List<SearchResponseKey> documents;
 }
