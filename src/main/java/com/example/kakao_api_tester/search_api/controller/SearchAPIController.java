5c2ec8dd22384365254bd6a7068c3f7467cba3fb
idooru <shere1765@gmail.com>
2023-10-15 12:56:21 +0900

    로깅 기능

--- src/main/java/com/example/kakao_api_tester/search_api/controller/SearchAPIController.java
+++ src/main/java/com/example/kakao_api_tester/search_api/controller/SearchAPIController.java
@@ -1,5 +1,6 @@
 package com.example.kakao_api_tester.search_api.controller;
 
+import com.example.kakao_api_tester.data.components.HttpTransactionLogger;
 import com.example.kakao_api_tester.data.dto.SearchRequestDto;
 import com.example.kakao_api_tester.data.dto.SearchResponseDto;
 import com.example.kakao_api_tester.data.type.SearchResponseJSON;
@@ -9,15 +10,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @AllArgsConstructor
 public class SearchAPIController {
 
     private final SearchAPIService searchAPIService;
+    private final HttpTransactionLogger logger;
 
     @GetMapping(value = "/search-api")
     public ResponseEntity<SearchResponseDto<SearchResponseJSON>> searchApi(SearchRequestDto searchRequestDto) {
+        logger.logRequestDto(searchRequestDto);
+        searchRequestDto.validateDto();
+
         SearchResponseJSON searchResponseJSON = searchAPIService.callSearchAPI(searchRequestDto);
+        logger.logResponseJson(searchResponseJSON);
 
         return searchAPIService.buildResponse(searchResponseJSON);
     }
