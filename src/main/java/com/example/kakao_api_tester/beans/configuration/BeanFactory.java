4863e7513bef5b939d1f41c7bc24974b51333987
idooru <shere1765@gmail.com>
2023-10-22 12:39:23 +0900

    HttpHeaders 빈 추가

--- src/main/java/com/example/kakao_api_tester/beans/configuration/BeanFactory.java
+++ src/main/java/com/example/kakao_api_tester/beans/configuration/BeanFactory.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class BeanFactory {
@@ -11,6 +12,11 @@ public class BeanFactory {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        return new HttpHeaders();
     }
 
 }
