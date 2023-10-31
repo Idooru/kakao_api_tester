ea7418a6e438c03aca32aad64fe6b09190bcad80
Idooru <shere1765@gmail.com>
2023-10-31 13:07:00 +0900

    롬복으로 생성자 만들기

--- src/main/java/com/example/kakao_api_tester/data/dto/SearchResponseDto.java
+++ src/main/java/com/example/kakao_api_tester/data/dto/SearchResponseDto.java
@@ -1,17 +1,12 @@
 package com.example.kakao_api_tester.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SearchResponseDto<T> {
     public boolean success;
     public String message;
     public T result;
-
-    public SearchResponseDto(boolean success, String message, T result) {
-        this.success = success;
-        this.message = message;
-        this.result = result;
-    }
-
 }
